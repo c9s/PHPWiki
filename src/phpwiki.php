3525e56915e1d49638726a2a22319f942b90bf2d
c9s <cornelius.howl@gmail.com>
2012-03-19 12:54:04 +0800

    First commit

---
+++ src/phpwiki.php
@@ -0,0 +1,131 @@
+<?php
+
+function getRelativePath($from, $to)
+{
+   $from = explode('/', $from);
+   $to = explode('/', $to);
+   foreach($from as $depth => $dir)
+   {
+
+        if(isset($to[$depth]))
+        {
+            if($dir === $to[$depth])
+            {
+               unset($to[$depth]);
+               unset($from[$depth]);
+            }
+            else
+            {
+               break;
+            }
+        }
+    }
+    //$rawresult = implode('/', $to);
+    for($i=0;$i<count($from)-1;$i++)
+    {
+        array_unshift($to,'..');
+    }
+    $result = implode('/', $to);
+    return $result;
+}
+
+function PhpMarkdown($text)
+{
+    // replace php tags with highlight string
+    $text = preg_replace('#(^<\?.*?\?>)#sme', 
+        '"\n\n<pre class=\"php\">" . str_replace("\n","",highlight_string(str_replace(\'\\"\',\'"\',\'$1\'),true)) . "</pre>\n\n" ', 
+        $text );
+    return Markdown($text);
+}
+
+if( empty($argv) || count($argv) < 3 ) {
+    die("Usage: phpwiki [input] [output]\n");
+}
+
+list($script,$input,$output) = $argv;
+
+// $output = 'wiki_html';
+if( ! file_exists($output) )
+    mkdir( $output , 0755 , true );
+
+$dirs = array($input);
+
+$wrapper =<<<HTML
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <style>
+    body { 
+        padding: 15px 30px; 
+        background-color: #bbb;
+        font-family: sans-serif;
+    }
+    p { 
+        color: #333;
+        text-shadow: #ccc 1px 1px 1px;
+    }
+    pre { 
+        background: #ddd;
+        border-radius: 10px;
+        border: 1px solid #999;
+        box-shadow: #666 -2px -2px 5px;
+        padding: 10px;
+    }
+    code { 
+        font-family: Monaco, Monospace, Courier New;
+        font-size: 0.8em;
+    }
+    </style>
+</head>
+<body>
+%BODY%
+</body>
+</html>
+HTML;
+
+echo "Generating wiki doc to $output ...\n";
+
+foreach( $dirs as $dir ) {
+
+    foreach (new RecursiveIteratorIterator(
+            new RecursiveDirectoryIterator($dir), 
+            RecursiveIteratorIterator::LEAVES_ONLY) as $file)
+    {
+        $ext = $file->getExtension();
+        if( $ext == 'mkd' || $ext == 'md' ) {
+
+
+            $filedir = $file->getPath();
+            $htmlFilename = $file->getBasename( '.'. $ext ) . '.html';
+            $subdir = dirname( substr( $file->getPathname() , strlen($dir) + 1 ) );
+            if( '.' == $subdir )  { 
+                $path = $output . DIRECTORY_SEPARATOR . $htmlFilename;
+            } else { 
+                $path = $output . DIRECTORY_SEPARATOR . $subdir . DIRECTORY_SEPARATOR . $htmlFilename;
+            }
+
+            $text = file_get_contents($file);
+
+            // re-filter wiki links
+            $text = preg_replace('#\[\[(.*?)\|(.*?)\]\]#xe',
+                " '<a href=\"' . getRelativePath('$path','$output/$1.html') . '\">' . '$2' . '</a>' ",
+                $text);
+
+            $text = preg_replace('#\[\[(.*?)\]\]#xe',
+                " '<a href=\"' . getRelativePath('$path','$output/$1.html') . '\">' . '$1' . '</a>' ",
+                $text);
+
+            $html = PhpMarkdown($text);
+            $html = str_replace('%BODY%', $html, $wrapper );
+
+            $d = dirname($path);
+            if( ! file_exists($d) ) 
+                mkdir( $d , 0755, true );
+
+            echo "\t" , $path , "\n";
+            file_put_contents( $path , $html );
+        }
+    }
+}
+echo "Done", "\n";
+
---
+++ wiki/index.md
@@ -0,0 +1,24 @@
+Index Page
+==========
+
+This is a [[link]]
+
+# Header I
+
+`keyboard` code
+
+    code
+    code
+    code
+
+## Header II
+
+### Header III
+
+PHP Code:
+
+<?php
+
+    $hello = "World";
+
+?>
